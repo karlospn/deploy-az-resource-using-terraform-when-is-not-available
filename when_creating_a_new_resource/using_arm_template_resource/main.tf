0c58acf57367c626bacd7c284a4e178763b8c07a
Carlos Pons <karlospn@gmail.com>
2022-11-01 20:23:30 +0100

    Add arm-template resource example

---
+++ when_creating_a_new_resource/using_arm_template_resource/main.tf
@@ -0,0 +1,44 @@
+locals {
+    resource_group_name = "rg-provisioning-demo"
+    app_conf_name = "appconf-demo-dev"
+    app_conf_sku = "free"
+    app_conf_public_network_access = "Enabled"
+    app_conf_disable_local_auth = false
+    app_conf_location = "westeurope"
+}
+
+## Create Resource Group
+resource "azurerm_resource_group" "rg_demo" {
+    name      = local.resource_group_name
+    location  = "West Europe"
+}
+
+## Create App Configuration using the AzApi provider
+resource "azurerm_resource_group_template_deployment" "appconf" {
+    name                  = "app-conf-deploy"
+    resource_group_name   = local.resource_group_name
+    deployment_mode       = "Incremental"
+    template_content      = file("template.json")
+    parameters_content  = <<PARAMETERS
+    {
+        "name": {
+            "value": "${local.app_conf_name}"
+        },
+        "location": {
+            "value": "${local.app_conf_location}"
+        },
+        "sku": {
+            "value": "${local.app_conf_sku}"
+        },
+        "public_network_access": {
+            "value": "${local.app_conf_public_network_access}"
+        },
+        "disable_local_auth": {
+            "value": "${local.app_conf_disable_local_auth}"
+        }
+    }
+    PARAMETERS 
+    depends_on = [
+        azurerm_resource_group.rg_demo
+    ]
+}
---
+++ when_creating_a_new_resource/using_arm_template_resource/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "local" {
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
